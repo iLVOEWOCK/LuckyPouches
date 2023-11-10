ac6f87a4d48803d3857f52a7266c5e09938af286
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-11-09 22:55:16 -0800

    Create Utils.php

---
+++ src/wockkinmycup/LuckyPouches/utils/Utils.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace wockkinmycup\LuckyPouches\utils;
+
+use pocketmine\entity\Entity;
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+class Utils {
+
+    /**
+     * Returns an online player whose name begins with or equals the given string (case insensitive).
+     * The closest match will be returned, or null if there are no online matches.
+     *
+     * @param string $name The prefix or name to match.
+     * @return Player|null The matched player or null if no match is found.
+     */
+    public static function customGetPlayerByPrefix(string $name): ?Player {
+        $found = null;
+        $name = strtolower($name);
+        $delta = PHP_INT_MAX;
+
+        /** @var Player[] $onlinePlayers */
+        $onlinePlayers = Server::getInstance()->getOnlinePlayers();
+
+        foreach ($onlinePlayers as $player) {
+            if (stripos($player->getName(), $name) === 0) {
+                $curDelta = strlen($player->getName()) - strlen($name);
+
+                if ($curDelta < $delta) {
+                    $found = $player;
+                    $delta = $curDelta;
+                }
+
+                if ($curDelta === 0) {
+                    break;
+                }
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * @param Entity $player
+     * @param string $sound
+     * @param int $volume
+     * @param int $pitch
+     * @param int $radius
+     */
+    public static function playSound(Entity $player, string $sound, $volume = 1, $pitch = 1, int $radius = 5): void
+    {
+        foreach ($player->getWorld()->getNearbyEntities($player->getBoundingBox()->expandedCopy($radius, $radius, $radius)) as $p) {
+            if ($p instanceof Player) {
+                if ($p->isOnline()) {
+                    $spk = new PlaySoundPacket();
+                    $spk->soundName = $sound;
+                    $spk->x = $p->getLocation()->getX();
+                    $spk->y = $p->getLocation()->getY();
+                    $spk->z = $p->getLocation()->getZ();
+                    $spk->volume = $volume;
+                    $spk->pitch = $pitch;
+                    $p->getNetworkSession()->sendDataPacket($spk);
+                }
+            }
+        }
+    }
+}
